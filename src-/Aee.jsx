14e4f6a3cf331e473a1f4d0724743715ee5657db
Ritika Lute <133960450+Ritikalute@users.noreply.github.com>
2024-06-01 17:54:40 +0530

    Add files via upload

---
+++ src-/Aee.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Aee() {
+  return (
+    <div>
+      <h1 className='text-center text-red-600 bg-slate-950'>HEllo Aee</h1>
+       {/* <div className='h-[130px] w-[166px] bg-slate-50  hover:border hover:border-2 hover:border-black'>
+                                <img src={Hcom2Image32} alt="" className='ml-20 h-auto w-auto mt-5' />
+                                <p className='ml-12 text-sm mt-3 font-semibold'>Legal document </p> 
+                                <p className='ml-12 text-sm font-semibold '>translation </p>
+                            </div>
+                            <div className='h-[130px] w-[166px] bg-slate-50  hover:border hover:border-2 hover:border-black'>
+                            <img src={Hcom2Image32} alt="" className='ml-20 h-auto w-auto mt-5' /> */}
+
+    </div>
+  )
+}
+
+export default Aee;
